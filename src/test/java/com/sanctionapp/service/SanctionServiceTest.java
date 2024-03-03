6a8b5c71de40b42e312f2ec7708e2a1057d58ee9
denitusha <denitusha3@gmail.com>
2024-03-03 22:54:03 +0100

    added some more test cases for algorithm

--- src/test/java/com/sanctionapp/service/SanctionServiceTest.java
+++ src/test/java/com/sanctionapp/service/SanctionServiceTest.java
@@ -39,31 +39,154 @@ public class SanctionServiceTest {
 
     @Mock
     private OurCustomComparator customComparator;
+
+    @Mock
+    private SearchHistoryService searchHistoryService;
     @InjectMocks
     private SanctionService sanctionService;
 
 
     @Test
-    void testSearchForSanctions() throws Exception {
-        // Mock the behavior of Environment and PersonCache
+    public void testSearchForSanctions1() {
+        sanctionTest(3,
+                Arrays.asList(
+                        new Person(1L, "John Doe", "john", "doe", "m", "m"),
+                        new Person(2L, "John Doe Smith", "john", "doe", "m", "m"),
+                        new Person(3L, "Jon DO", "john", "doe", "m", "m")
+                ),
+                "John Doe",
+                3);
+    }
+    @Test
+    public void testSearchForSanctions2() {
+        List<Person> personList = Arrays.asList(
+                new Person(1L, "Bank of Kosova Kosova", "John", "Doe", "m", "Al"),
+                new Person(2L, "Bank of Kosova", "John", "Doe", "m", "Al"),
+                new Person(3L, "Bank of kongo", "John", "Doe", "m", "Al")
+        );
+        sanctionTest(3, personList, "Bank of Kosova", 2);
 
-        when(environment.getProperty("max-matches", Integer.class, 3)).thenReturn(3);
-        when(environment.getProperty("threshold", Integer.class, 85)).thenReturn(85);
-        when(personCache.size()).thenReturn(4L); // Mocking size as 4
-        when(personCache.get(0L)).thenReturn(new Person(1L, "John Doe", "john", "doe", "m", "m"));
-        when(personCache.get(1L)).thenReturn(new Person(2L, "Jsadaewn Doe", "john", "doe", "m", "m"));
-        when(personCache.get(2L)).thenReturn(new Person(3L, "John Doe", "john", "doe", "m", "m"));
-        when(personCache.get(3L)).thenReturn(new Person(4L, "John doe", "john", "doe", "m", "m"));
-
-
-        // Call the method under test
-        Response response = sanctionService.searchForSanctions("John Doe");
-
-        System.out.println(response);
-        // Assert the size of the response matches
-        assertEquals(3, response.getMatches().size());
     }
 
 
 
+
+
+
+    @Test
+    public void testSearchForSanctions3() {
+
+        List<Person> personList = Arrays.asList(
+                new Person(1L, "Kejda Simoni", "John", "Doe", "m", "Al")
+
+        );
+
+        sanctionTest(1, personList, "Kieeda  Simjoni", 1);
+
+
+    }
+
+    @Test
+    public void testSearchForSanctions4() {
+
+        List<Person> personList = Arrays.asList(
+                new Person(1L, "Kejda Simoni", "John", "Doe", "m", "Al")
+
+        );
+
+        sanctionTest(1, personList, "Keeida Simjoni", 1);
+
+    }
+
+    @Test
+    public void testSearchForSanctions5() {
+
+        List<Person> personList = Arrays.asList(
+                new Person(1L, "Kejda Simoni", "John", "Doe", "m", "Al")
+
+        );
+
+        sanctionTest(1, personList, "Keeida asd Simjoni", 1);
+
+    }
+
+
+    @Test
+    public void testSearchForSanctions6() {
+
+        List<Person> personList = Arrays.asList(
+                new Person(1L, "Kejda Simoni", "John", "Doe", "m", "Al")
+
+        );
+
+        sanctionTest(1, personList, "Keila Simani", 1);
+    }
+
+    @Test
+    public void testSearchForSanctions7() {
+
+        List<Person> personList = Arrays.asList(
+                new Person(1L, "Kejda Simoni", "John", "Doe", "m", "Al")
+
+        );
+
+        sanctionTest(1, personList, "Kejda Simani", 1);
+
+    }
+
+    @Test
+    public void testSearchForSanctions8() {
+
+        List<Person> personList = Arrays.asList(
+                new Person(1L, "Kejda Simoni", "John", "Doe", "m", "Al")
+
+        );
+        sanctionTest(1, personList, "Keeeejda Simoni", 1);
+
+    }
+
+
+    @Test
+    public void testSearchForSanctions9() {
+
+        List<Person> personList = Arrays.asList(
+                new Person(1L, "Kejda Simoni", "John", "Doe", "m", "Al")
+
+        );
+
+        sanctionTest(1, personList, "Simoni kejda", 1);
+    }
+
+    @Test
+    void testSearchForSanctions10() throws Exception {
+        sanctionTest(4,
+                Arrays.asList(
+                        new Person(1L, "John Doe", "john", "doe", "m", "m"),
+                        new Person(2L, "John Doe", "john", "doe", "m", "m"),
+                        new Person(3L, "John Doe", "john", "doe", "m", "m"),
+                        new Person(4L, "John doe", "john", "doe", "m", "m")
+                ),
+                "John Doe",
+                3);
+    }
+
+
+    private void sanctionTest(int cacheSize, List<Person> persons, String searchNamed, int expectedSize) {
+        when(environment.getProperty("max-matches", Integer.class, 3)).thenReturn(3);
+        when(environment.getProperty("threshold", Integer.class, 85)).thenReturn(85);
+        when(personCache.size()).thenReturn((long) cacheSize); // Mocking size as 4
+        for (int i = 0; i < cacheSize; i++) {
+            when(personCache.get((long) i)).thenReturn(persons.get(i));
+        }
+
+        Response response = sanctionService.searchForSanctions(searchNamed);
+
+        assertEquals(expectedSize, response.getMatches().size());
+    }
+
 }
+
+
+
+
+
